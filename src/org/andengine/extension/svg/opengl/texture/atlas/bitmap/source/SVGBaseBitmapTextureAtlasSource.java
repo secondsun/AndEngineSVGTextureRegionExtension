205b05246d1f60bb32fd17c7777970b5876dcf29
Summers Pittman <secondsun@gmail.com>
2014-04-14 17:30:32 -0400

    Updated for GLES2-AnchorCenter

--- src/org/andengine/extension/svg/opengl/texture/atlas/bitmap/source/SVGBaseBitmapTextureAtlasSource.java
+++ src/org/andengine/extension/svg/opengl/texture/atlas/bitmap/source/SVGBaseBitmapTextureAtlasSource.java
@@ -1,73 +1,83 @@
 package org.andengine.extension.svg.opengl.texture.atlas.bitmap.source;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Picture;
 import org.andengine.extension.svg.adt.SVG;
 import org.andengine.opengl.texture.atlas.bitmap.source.PictureBitmapTextureAtlasSource;
+import org.andengine.util.BitmapUtils;
 import org.andengine.util.debug.Debug;
 
-
 /**
- * (c) 2010 Nicolas Gramlich 
- * (c) 2011 Zynga Inc.
- * 
+ * (c) 2010 Nicolas Gramlich (c) 2011 Zynga Inc.
+ *
  * @author Nicolas Gramlich
  * @since 13:34:55 - 21.05.2011
  */
 public class SVGBaseBitmapTextureAtlasSource extends PictureBitmapTextureAtlasSource {
 	// ===========================================================
-	// Constants
-	// ===========================================================
+    // Constants
+    // ===========================================================
 
-	// ===========================================================
-	// Fields
-	// ===========================================================
+    // ===========================================================
+    // Fields
+    // ===========================================================
+    private final SVG mSVG;
 
-	private final SVG mSVG;
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+    public SVGBaseBitmapTextureAtlasSource(final SVG pSVG) {
+        this(pSVG, 0, 0);
+    }
 
-	// ===========================================================
-	// Constructors
-	// ===========================================================
+    public SVGBaseBitmapTextureAtlasSource(final SVG pSVG, final float pScale) {
+        this(pSVG, 0, 0, pScale);
+    }
 
-	public SVGBaseBitmapTextureAtlasSource(final SVG pSVG) {
-		this(pSVG, 0, 0);
-	}
+    public SVGBaseBitmapTextureAtlasSource(final SVG pSVG, final int pTextureX, final int pTextureY, final float pScale) {
+        super(pSVG.getPicture(), pTextureX, pTextureY, pScale);
+        this.mSVG = pSVG;
+    }
 
-	public SVGBaseBitmapTextureAtlasSource(final SVG pSVG, final float pScale) {
-		this(pSVG, 0, 0, pScale);
-	}
+    public SVGBaseBitmapTextureAtlasSource(final SVG pSVG, final int pWidth, final int pHeight) {
+        this(pSVG, 0, 0, pWidth, pHeight);
+    }
 
-	public SVGBaseBitmapTextureAtlasSource(final SVG pSVG, final int pTextureX, final int pTextureY, final float pScale) {
-		super(pSVG.getPicture(), pTextureX, pTextureY, pScale);
-		this.mSVG = pSVG;
-	}
+    public SVGBaseBitmapTextureAtlasSource(final SVG pSVG, final int pTextureX, final int pTextureY, final int pTextureWidth, final int pTextureHeight) {
+        super(pSVG.getPicture(), pTextureX, pTextureY, pTextureWidth, pTextureHeight);
+        this.mSVG = pSVG;
+    }
 
-	public SVGBaseBitmapTextureAtlasSource(final SVG pSVG, final int pWidth, final int pHeight) {
-		this(pSVG, 0, 0, pWidth, pHeight);
-	}
+    @Override
+    public SVGBaseBitmapTextureAtlasSource deepCopy() {
+        Debug.w(this.getClass().getSimpleName() + ".deepCopy() does not actually deepCopy the SVG!");
+        return new SVGBaseBitmapTextureAtlasSource(this.mSVG, this.mTextureX, this.mTextureY, this.mTextureWidth, this.mTextureHeight);
+    }
 
-	public SVGBaseBitmapTextureAtlasSource(final SVG pSVG, final int pTextureX, final int pTextureY, final int pTextureWidth, final int pTextureHeight) {
-		super(pSVG.getPicture(), pTextureX, pTextureY, pTextureWidth, pTextureHeight);
-		this.mSVG = pSVG;
-	}
+    // ===========================================================
+    // Getter & Setter
+    // ===========================================================
+    // ===========================================================
+    // Methods for/from SuperClass/Interfaces
+    // ===========================================================
+    // ===========================================================
+    // Methods
+    // ===========================================================
+    // ===========================================================
+    // Inner and Anonymous Classes
+    // ===========================================================
+    @Override
+    public Bitmap onLoadBitmap(Bitmap.Config pBitmapConfig, boolean pMutable) {
 
-	@Override
-	public SVGBaseBitmapTextureAtlasSource deepCopy() {
-		Debug.w(this.getClass().getSimpleName() + ".deepCopy() does not actually deepCopy the SVG!");
-		return new SVGBaseBitmapTextureAtlasSource(this.mSVG, this.mTextureX, this.mTextureY, this.mTextureWidth, this.mTextureHeight);
-	}
+        final Bitmap bitmap = onLoadBitmap(pBitmapConfig);
 
-	// ===========================================================
-	// Getter & Setter
-	// ===========================================================
+        if (pMutable) {
+            return BitmapUtils.ensureBitmapIsMutable(bitmap);
+        } else {
+            return bitmap;
+        }
 
-	// ===========================================================
-	// Methods for/from SuperClass/Interfaces
-	// ===========================================================
+    }
 
-	// ===========================================================
-	// Methods
-	// ===========================================================
-
-	// ===========================================================
-	// Inner and Anonymous Classes
-	// ===========================================================
 }
